e16ec9c1d6755853f64d4c45217f7555c51c203b
mmzsako <91384022+mmzsako@users.noreply.github.com>
2021-10-09 13:01:16 +0200

    zadanie 5.

--- js/functions.js
+++ js/functions.js
@@ -1,9 +1,38 @@
-function printMessage(msg){
+function printMessage(msg) {
 	let div = document.createElement('div');
 	div.innerHTML = msg;
 	document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
+function clearMessages() {
 	document.getElementById('messages').innerHTML = '';
+}
+
+function getMoveName(argMoveId) {
+	if (argMoveId == '1') {
+		return 'kamień';
+	} else if (argMoveId == '2') {
+		return 'papier';
+	} else if (argMoveId == '3') {
+		return 'nożyce';
+	} else {
+		printMessage('Nie znam ruchu o id ' + argMoveId + '.');
+		return 'nieznany ruch';
+	}
+}
+
+function displayResult(pcMove, playerMove) {
+	printMessage('Zagrałem ' + pcMove + ', a Ty ' + playerMove);
+
+	if ((playerMove === 'papier' && pcMove === 'kamień') || (playerMove === 'kamień' && pcMove === 'nożyce') || (playerMove === 'nożyce' && pcMove === 'papier')) { // win
+		printMessage(`Wygrałeś! Twój ruch - ${playerMove}, ruch komputera - ${pcMove}`)
+	} else if ((playerMove === 'kamień' && pcMove === 'papier') || (playerMove === 'nożyce' && pcMove === 'kamień') || (playerMove === 'papier' && pcMove === 'nożyce')) { // failure
+		printMessage(`Przegałeś! Twój ruch - ${playerMove}, ruch komputera - ${pcMove}`)
+	} else if((playerMove === 'kamień' && pcMove === 'kamień') || (playerMove === 'nożyce' && pcMove === 'nożyce') || (playerMove === 'papier' && pcMove === 'papier')){
+		printMessage(`Remis! twój ruch to - ${playerMove}, ruch komputera - ${pcMove}`)
+	} else {
+		printMessage('Błedne dane, mecz nie rozegrany :(');
+	}
+
+
 }
--- js/script.js
+++ js/script.js
@@ -1,15 +1,7 @@
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 
-let playerMove = 'nieznany ruch';
-
-if (playerInput == '1') {
-    playerMove = 'kamień';
-} else if (playerInput == '2') {
-    playerMove = 'papier';
-} else if (playerInput == '3') {
-    playerMove = 'nożyce';
-}
+let playerMove = getMoveName(playerInput);
 
 printMessage('Twój ruch to: ' + playerMove);
 
@@ -19,15 +11,5 @@ const pcMove = moves[pcMoveIndex];
 
 printMessage('Ruch komputera to: ' + pcMove);
 
-if ((playerMove === 'papier' && pcMove === 'kamień') || (playerMove === 'kamień' && pcMove === 'nożyce') || (playerMove === 'nożyce' && pcMove === 'papier')) { // win
-    printMessage(`ty wygrałeś! twój ruch - ${playerMove}, ruch komputera - ${pcMove}`)
-} else if ((playerMove === 'kamień' && pcMove === 'papier') || (playerMove === 'nożyce' && pcMove === 'kamień') || (playerMove === 'papier' && pcMove === 'scissors')) { // failure
-    printMessage(`ty przegałeś! twój ruch - ${playerMove}, ruch komputera - ${pcMove}`)
-} else {
-    printMessage(`remis! twój ruch to - ${playerMove}, ruch komputera - ${pcMove}`)
-} 
-
-
-
-
+displayResult(pcMove, playerMove )
 
